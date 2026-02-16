9811b6074a843c9aee35d7f6d94f5ec7d1a684f3
Sharo <sharo0402@hotmail.com>
2026-02-16 22:54:17 +0100

    2023-06-10T12:00:00Z

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/style.css';
-import App from '/App';
+import './style/styles.css';
+import App from './/App';
 
 ReactDOM.render(
   <React.StrictMode>
